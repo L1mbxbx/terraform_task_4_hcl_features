998a2b9f59e0160e30178035c674e26524323244
L1mbxbx <ilya.msd2003@gmail.com>
2025-08-04 22:02:35 +0300

    Add dynamic block for network security group, fix typo

--- locals.tf
+++ locals.tf
@@ -1,6 +1,12 @@
 locals {
   nic_names = ["nic1", "nic2"]
 
+  network_interface_names = [
+    "${var.prefix}-nic-1",
+    "${var.prefix}-nic-2",
+    "${var.prefix}-nic-3"
+  ]
+  
   security_rules = [
     {
       name                       = "SSH"
@@ -27,10 +33,4 @@ locals {
   ]
 }
 
-locals {
-  network_interface_names = [
-    "{var.prefix}-nic-1",
-    "{var.prefix}-nic-2",
-    "{var.prefix}-nic-3"
-  ]
-}
+
